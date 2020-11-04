761eb452b91823192f366c8f09cdc856737b9374
Claudio <claudiocli.cp@gmail.com>
2020-11-04 12:01:10 +0100

    Implemented Test
    Test implemented, with the add of JodaTime library to track tests' time.
    Minor fixes on MyListUtil

--- src/test/java/MyListUtilTest.java
+++ src/test/java/MyListUtilTest.java
@@ -1,5 +1,60 @@
+import static org.junit.Assert.assertArrayEquals;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class MyListUtilTest
 {
+
+	private static MyListUtil l;
+	private static Object[] arrayCrescente;
+	private static Object[] arrayDecrescente;
+
+	@BeforeClass
+	public static void setup()	{
+		l=new MyListUtil();
+		arrayCrescente=new Object[5];
+		arrayDecrescente=new Object[5];
+		System.out.println("Tests started at:\n"+LocalDate.now()+" "
+				+LocalTime.now());
+	}
+
+	@AfterClass
+	public static void finish()	{
+		System.out.println("Tests ended at:\n"+LocalDate.now()+" "
+				+LocalTime.now());
+	}
+
+	@Before
+	public void insertElemnts()	{
+		for (int i=0; i<5; i++)
+		{
+			l.getList().add(i);
+			arrayCrescente[i]=i;
+			arrayDecrescente[i]=4-i;
+		}
+	}
+
+	@After
+	public void eraseElements()	{
+		l.getList().clear();
+	}
+
+	@Test
+	public void ordineCrescenteWorks()	{
+		l.ordinaLista(true);
+		assertArrayEquals(arrayCrescente, l.getList().toArray());
+	}
+
+	@Test
+	public void ordineDecrescenteWorks()	{
+		l.ordinaLista(false);
+		assertArrayEquals(arrayDecrescente, l.getList().toArray());
+	}
 
 }
